5883c109d8ad9b82b45493a57db676e833ba8909
Camila <N00235579@upn.pe>
2022-06-02 20:19:57 -0500

    conexion

--- proyectoSemana10TemaFormulariosTareaDeSemanal/Melissa.Designer.cs
+++ proyectoSemana10TemaFormulariosTareaDeSemanal/Melissa.Designer.cs
@@ -37,6 +37,8 @@
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnLimpiar = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -44,9 +46,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(79, 121);
+            this.label1.Location = new System.Drawing.Point(105, 149);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 17);
+            this.label1.Size = new System.Drawing.Size(177, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre del producto:";
             // 
@@ -55,9 +58,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(79, 172);
+            this.label2.Location = new System.Drawing.Point(105, 212);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.Size = new System.Drawing.Size(168, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "Codigo del producto:";
             // 
@@ -66,40 +70,45 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(79, 229);
+            this.label3.Location = new System.Drawing.Point(105, 282);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(132, 17);
+            this.label3.Size = new System.Drawing.Size(161, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "Precio del producto:";
             // 
             // txtNombreP
             // 
-            this.txtNombreP.Location = new System.Drawing.Point(263, 118);
+            this.txtNombreP.Location = new System.Drawing.Point(351, 145);
+            this.txtNombreP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNombreP.Name = "txtNombreP";
-            this.txtNombreP.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreP.Size = new System.Drawing.Size(132, 22);
             this.txtNombreP.TabIndex = 3;
             // 
             // txtCodigoP
             // 
-            this.txtCodigoP.Location = new System.Drawing.Point(263, 172);
+            this.txtCodigoP.Location = new System.Drawing.Point(351, 212);
+            this.txtCodigoP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCodigoP.Name = "txtCodigoP";
-            this.txtCodigoP.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigoP.Size = new System.Drawing.Size(132, 22);
             this.txtCodigoP.TabIndex = 4;
             // 
             // txtPrecioP
             // 
-            this.txtPrecioP.Location = new System.Drawing.Point(263, 229);
+            this.txtPrecioP.Location = new System.Drawing.Point(351, 282);
+            this.txtPrecioP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPrecioP.Name = "txtPrecioP";
-            this.txtPrecioP.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecioP.Size = new System.Drawing.Size(132, 22);
             this.txtPrecioP.TabIndex = 5;
             this.txtPrecioP.TextChanged += new System.EventHandler(this.txtPrecioP_TextChanged);
             this.txtPrecioP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecioP_KeyPress);
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(136, 293);
+            this.btnAgregar.Location = new System.Drawing.Point(181, 361);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.Size = new System.Drawing.Size(100, 28);
             this.btnAgregar.TabIndex = 6;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
@@ -107,9 +116,10 @@
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Location = new System.Drawing.Point(254, 293);
+            this.btnLimpiar.Location = new System.Drawing.Point(339, 361);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 28);
             this.btnLimpiar.TabIndex = 7;
             this.btnLimpiar.Text = "Limpiar";
             this.btnLimpiar.UseVisualStyleBackColor = true;
@@ -119,18 +129,30 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(49, 62);
+            this.label4.Location = new System.Drawing.Point(65, 76);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(356, 22);
+            this.label4.Size = new System.Drawing.Size(442, 27);
             this.label4.TabIndex = 8;
             this.label4.Text = "Ingrese la información del nuevo producto";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(533, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(316, 228);
+            this.dataGridView1.TabIndex = 9;
+            // 
             // Melissa
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::proyectoSemana10TemaFormulariosTareaDeSemanal.Properties.Resources._new;
-            this.ClientSize = new System.Drawing.Size(456, 378);
+            this.ClientSize = new System.Drawing.Size(876, 465);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.btnAgregar);
@@ -140,8 +162,10 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Melissa";
             this.Text = "Agregar Producto";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,5 +182,6 @@
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnLimpiar;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
