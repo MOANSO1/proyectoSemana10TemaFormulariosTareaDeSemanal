aa537467e44dbe1d32fbc3b3e61eb67d14592fcf
Camila <N00235579@upn.pe>
2022-06-02 20:56:56 -0500

    saul form

---
+++ proyectoSemana10TemaFormulariosTareaDeSemanal/Saul.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace proyectoSemana10TemaFormulariosTareaDeSemanal
+{
+    partial class Saul
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnRestaurar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.txtELaboral = new System.Windows.Forms.TextBox();
+            this.txtHijos = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtFNacimiento = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(790, 729);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(220, 75);
+            this.btnSalir.TabIndex = 50;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnRestaurar
+            // 
+            this.btnRestaurar.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestaurar.Location = new System.Drawing.Point(425, 729);
+            this.btnRestaurar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRestaurar.Name = "btnRestaurar";
+            this.btnRestaurar.Size = new System.Drawing.Size(220, 75);
+            this.btnRestaurar.TabIndex = 49;
+            this.btnRestaurar.Text = "Restaurar";
+            this.btnRestaurar.UseVisualStyleBackColor = true;
+            this.btnRestaurar.Click += new System.EventHandler(this.btnRestaurar_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(68, 729);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(220, 75);
+            this.btnRegistrar.TabIndex = 48;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(413, 636);
+            this.txtSueldo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSueldo.Multiline = true;
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(191, 40);
+            this.txtSueldo.TabIndex = 47;
+            // 
+            // txtELaboral
+            // 
+            this.txtELaboral.Location = new System.Drawing.Point(413, 534);
+            this.txtELaboral.Margin = new System.Windows.Forms.Padding(4);
+            this.txtELaboral.Multiline = true;
+            this.txtELaboral.Name = "txtELaboral";
+            this.txtELaboral.Size = new System.Drawing.Size(191, 40);
+            this.txtELaboral.TabIndex = 46;
+            // 
+            // txtHijos
+            // 
+            this.txtHijos.Location = new System.Drawing.Point(413, 417);
+            this.txtHijos.Margin = new System.Windows.Forms.Padding(4);
+            this.txtHijos.Multiline = true;
+            this.txtHijos.Name = "txtHijos";
+            this.txtHijos.Size = new System.Drawing.Size(191, 40);
+            this.txtHijos.TabIndex = 45;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(413, 302);
+            this.txtDireccion.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDireccion.Multiline = true;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(613, 40);
+            this.txtDireccion.TabIndex = 44;
+            // 
+            // txtFNacimiento
+            // 
+            this.txtFNacimiento.Location = new System.Drawing.Point(413, 203);
+            this.txtFNacimiento.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFNacimiento.Multiline = true;
+            this.txtFNacimiento.Name = "txtFNacimiento";
+            this.txtFNacimiento.Size = new System.Drawing.Size(191, 40);
+            this.txtFNacimiento.TabIndex = 43;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(413, 110);
+            this.txtEdad.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEdad.Multiline = true;
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(191, 40);
+            this.txtEdad.TabIndex = 42;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(413, 28);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(613, 40);
+            this.txtNombre.TabIndex = 41;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(34, 636);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 27);
+            this.label7.TabIndex = 40;
+            this.label7.Text = "SUELDO FIJO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(34, 534);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(326, 27);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "EXPERIENCIA LABORAL (Si/No)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 417);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 27);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "HIJOS (Cantidad)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 302);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 27);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "DIRECCIÓN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 203);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(254, 27);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "FECHA DE NACIMIENTO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 110);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 27);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "EDAD";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 27);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "NOMBRE";
+            // 
+            // Saul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1068, 865);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnRestaurar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtSueldo);
+            this.Controls.Add(this.txtELaboral);
+            this.Controls.Add(this.txtHijos);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtFNacimiento);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Saul";
+            this.Text = "Saul";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnRestaurar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.TextBox txtELaboral;
+        private System.Windows.Forms.TextBox txtHijos;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtFNacimiento;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ proyectoSemana10TemaFormulariosTareaDeSemanal/Saul.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoSemana10TemaFormulariosTareaDeSemanal
+{
+    public partial class Saul : Form
+    {
+        public Saul()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            int edad = Convert.ToInt16(txtEdad.Text);
+            string fnacimiento = txtFNacimiento.Text;
+            string direccion = txtDireccion.Text;
+            int hijos = Convert.ToInt16(txtHijos.Text);
+            string elaboral = txtELaboral.Text;
+            double sueldo = Convert.ToDouble(txtSueldo.Text);
+            if (nombre == "" || fnacimiento == "" || direccion == "" || elaboral == "")
+            {
+                MessageBox.Show("Ingrese todos los datos requeridos");
+                return;
+            }
+            if (!int.TryParse(txtEdad.Text, out edad) || !int.TryParse(txtHijos.Text, out hijos) || !double.TryParse(txtSueldo.Text, out sueldo))
+            {
+                txtEdad.Text = "";
+                txtHijos.Text = "";
+                txtSueldo.Text = "";
+                MessageBox.Show("Ingrese sólo valores numericos en los espacios correspondientes ");
+                return;
+            }
+            MessageBox.Show("Empleado Registrado correctamente");
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            txtNombre.ResetText();
+            txtEdad.ResetText();
+            txtFNacimiento.ResetText();
+            txtDireccion.ResetText();
+            txtHijos.ResetText();
+            txtELaboral.ResetText();
+            txtSueldo.ResetText();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
