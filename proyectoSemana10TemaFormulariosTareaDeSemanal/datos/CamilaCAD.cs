ebb7203af54cfd55e8ab1cb9d9f1f1f6cc547baa
Camila <N00235579@upn.pe>
2022-06-02 20:25:40 -0500

    cao y cs Camila

---
+++ proyectoSemana10TemaFormulariosTareaDeSemanal/datos/CamilaCAD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoSemana10TemaFormulariosTareaDeSemanal.datos
+{
+    class CamilaCAD
+    {
+        public static bool guardar(Camila c)
+        {
+
+        }
+    }
+}
